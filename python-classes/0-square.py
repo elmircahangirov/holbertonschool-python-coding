a0319b6261ba3c9429ae790756e6bbd004322b8f
elmircahangirov <elmir.cahangir@gmail.com>
2026-03-27 08:53:37 +0000

    task 0 is done!

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size):
+        self.__size = size
